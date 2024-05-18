ee88bee167208e0456c17032483b5558029395df
zjhlogo <zjhlogo@gmail.com>
2024-05-18 19:32:08 +0800

    fix some bugs

--- Source/Survivor/SurvivorGameMode.cpp
+++ Source/Survivor/SurvivorGameMode.cpp
@@ -10,17 +10,17 @@ ASurvivorGameMode::ASurvivorGameMode()
 	// use our custom PlayerController class
 	PlayerControllerClass = ASurvivorPlayerController::StaticClass();
 
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
-
-	// set default controller to our Blueprinted controller
-	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
-	if(PlayerControllerBPClass.Class != nullptr)
-	{
-		PlayerControllerClass = PlayerControllerBPClass.Class;
-	}
+	// // set default pawn class to our Blueprinted character
+	// static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/BP/Character/BP_TopDownCharacter.BP_TopDownCharacter"));
+	// if (PlayerPawnBPClass.Class != nullptr)
+	// {
+	// 	DefaultPawnClass = PlayerPawnBPClass.Class;
+	// }
+	//
+	// // set default controller to our Blueprinted controller
+	// static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/BP/Character/BP_TopDownPlayerController.BP_TopDownPlayerController"));
+	// if(PlayerControllerBPClass.Class != nullptr)
+	// {
+	// 	PlayerControllerClass = PlayerControllerBPClass.Class;
+	// }
 }
